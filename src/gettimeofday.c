daf13314c220831327ef67edca4cb92db64a4301
piers <piers@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2009-06-04 14:03:43 +0000

    Added cast to (long) for conversion from windows time format - to shut up warnings from compiler.

--- src/gettimeofday.c
+++ src/gettimeofday.c
@@ -9,7 +9,7 @@ int gettimeofday(struct timeval *tp, void *tz)
 	struct _timeb timebuffer;   
 
 	_ftime( &timebuffer );
-	tp->tv_sec  = timebuffer.time;
+	tp->tv_sec  = (long)timebuffer.time;
 	tp->tv_usec = timebuffer.millitm * 1000;
 	return 0;
 }
